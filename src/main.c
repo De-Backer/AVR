644e9d3c3ab18dde33330b3adbb68b28f132a431
Simon De Backer <simondebacker@msn.com>
2021-03-22 18:20:21 +0100

    Eerste test

--- src/main.c
+++ src/main.c
@@ -36,7 +36,7 @@ extern "C"
          * 0 = ingang
          * 1 = uitgang
          **/
-        DDRA = 0b00000000;
+        DDRA = 0b11111111;
         DDRB = 0b00000000;
         DDRC = 0b00000001;
         DDRD = 0b00000000;
@@ -89,30 +89,75 @@ extern "C"
             int8_t RX_BufferCount = RingBuffer_GetCount(&RX_Buffer);
             if (RX_BufferCount > 0)
             {
-                CAN_TX_msg.id     = 50;
-                CAN_TX_msg.length = 0;
-                while (RX_BufferCount--)
-                {
+                CAN_TX_msg.id           = 0x501;
+                CAN_TX_msg.length       = 3;
+                CAN_TX_msg.data_byte[0] = 04;
+                CAN_TX_msg.data_byte[1] = 00;
+                CAN_TX_msg.data_byte[2] = RingBuffer_Remove(&RX_Buffer);
 
-                    CAN_TX_msg.data_byte[CAN_TX_msg.length++] =
-                        RingBuffer_Remove(&RX_Buffer);
-                    if (CAN_TX_msg.length > 7
-                        || (RX_BufferCount == 0 && CAN_TX_msg.length > 0))
-                    {
-                        MCP2515_message_TX();
-                        uint8_t temp = 0;
-                        for (; temp < CAN_TX_msg.length; ++temp)
-                        { Transmit_USART0(CAN_TX_msg.data_byte[temp]); }
+                MCP2515_message_TX();
 
-                        ++CAN_TX_msg.id;
-                        CAN_TX_msg.length = 0;
-                    }
-                }
+                //                while (RX_BufferCount--)
+                //                {
+
+                //                    CAN_TX_msg.data_byte[CAN_TX_msg.length++]
+                //                    =
+                //                        RingBuffer_Remove(&RX_Buffer);
+                //                    if (CAN_TX_msg.length > 7
+                //                        || (RX_BufferCount == 0 &&
+                //                        CAN_TX_msg.length > 0))
+                //                    {
+                //                        MCP2515_message_TX();
+                //                        uint8_t temp = 0;
+                //                        for (; temp < CAN_TX_msg.length;
+                //                        ++temp) {
+                //                        Transmit_USART0(CAN_TX_msg.data_byte[temp]);
+                //                        }
+
+                //                        ++CAN_TX_msg.id;
+                //                        CAN_TX_msg.length = 0;
+                //                    }
+                //                }
             }
             if (MCP2515_check_for_incoming_message())
             {
                 if (MCP2515_message_RX())
                 {
+                    /* start van protocol
+                     * µc   ID 5       01
+                     * DCA  data_byte1 04
+                     * port data_byte2 00
+                     * var  data_byte3 00-ff
+                     */
+                    if (CAN_RX_msg.id == 0x501)
+                    {
+                        if (CAN_RX_msg.length == 3)
+                        {
+                            if (CAN_RX_msg.data_byte[0] == 0x04)
+                            {
+                                if (CAN_RX_msg.data_byte[1] == 0x00)
+                                {
+                                    PORTA = CAN_RX_msg.data_byte[2];
+
+                                    char* Buffer = "set DAC to";
+                                    Transmit_USART0(10); /* new line */
+                                    Transmit_USART0(10); /* new line */
+
+                                    while (*Buffer)
+                                    { Transmit_USART0(*Buffer++); }
+
+                                    Buffer = "0000"; /* clean buffer */
+                                    itoa(PORTA, Buffer, 10);
+                                    while (*Buffer)
+                                    { Transmit_USART0(*Buffer++); }
+
+                                    Transmit_USART0(10); /* new line */
+                                    Transmit_USART0(10); /* new line */
+                                }
+                            }
+                        }
+                    }
+
                     uint8_t temp = 0;
                     for (; temp < CAN_RX_msg.length; ++temp)
                     { Transmit_USART0(CAN_RX_msg.data_byte[temp]); }
