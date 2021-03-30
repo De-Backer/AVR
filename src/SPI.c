6a5f9ebbae53d49cdfbb71e14968f8d705e27f16
Simon De Backer <simondebacker@msn.com>
2021-03-30 23:10:35 +0200

    Mod SPI clock rate to 5MHz
    CAN controller miste data
    SPI was te traag

--- src/SPI.c
+++ src/SPI.c
@@ -7,11 +7,11 @@ void init_SPI()
         (0x01 << SPI_CS) | (0x01 << SPI_SCK) | (0x01 << SPI_MOSI); /* output */
     SPI_DDR &= ~(0x01 << SPI_MISO);                                /* input */
     /* Enable SPI, Master, set clock rate fck/128 */
-    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0) | (1 << SPR1);
+    //    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0) | (1 << SPR1);
     /* Enable SPI, Master, set clock rate fck/16 */
     //	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
     /* Enable SPI, Master, set clock rate fck/4 */
-    //    SPCR = (1 << SPE) | (1 << MSTR);
+    SPCR = (1 << SPE) | (1 << MSTR);
     /* Enable SPI, Master, set clock rate fck/2 */
     //    SPCR = (1 << SPE) | (1 << MSTR);
     //    SPSR |= (1 << SPI2X);
