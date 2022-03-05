9a058b71646715854d3978176e80e77f1a3b0241
Simon De Backer <simondebacker@msn.com>
2022-03-05 09:55:10 +0100

    Mod ADC add 8bit read

--- include/adc.h
+++ include/adc.h
@@ -10,6 +10,7 @@ extern "C"
 #include <avr/interrupt.h>
 #include <avr/io.h>
     void init_ADC();
+    uint8_t ADC_value_8bit(uint8_t source);
     uint16_t ADC_value(uint8_t source);
 
     ISR(ADC_vect);
--- src/adc.c
+++ src/adc.c
@@ -1,6 +1,6 @@
 #include "../include/adc.h"
 
-#include <util/delay.h>
+//#include <util/delay.h>
 
 void init_ADC()
 {
@@ -8,20 +8,40 @@ void init_ADC()
     DIDR0 = 0xff;
 
     /* ADC Multiplexer Selection Register */
-    /* AREF, Internal Vref turned 2.56V */
-    ADMUX = (0x01 << REFS1) | (0x01 << REFS0);
+        /* AREF, Internal Vref turned 2.56V */
+        //ADMUX = (0x01 << REFS1) | (0x01 << REFS0);
 
-    /* MUX4:0: Analog Channel and Gain Selection Bits => ADC0 */
-    /* ADLAR ADC Left Adjust Result */
-    ADMUX |= (0x01 << ADLAR);
+        /* AVCC with external capacitor at AREF pin */
+        //ADMUX=(0x01 << REFS0);
+
+        /* MUX4:0: Analog Channel and Gain Selection Bits => ADC0 */
+        /* ADLAR ADC Left Adjust Result */
+        //ADMUX |= (0x01 << ADLAR);
+
+        ADMUX = (0x01 << ADLAR);
 
     /* ADC Control and Status Register A */
-    /* ADPS2:0: ADC Prescaler Select Bits 156.25KHz sample rate @ 20MHz */
-    ADCSRA = (0x01 << ADPS2) | (0x01 << ADPS1) | (0x01 << ADPS0);
-    /* ADEN: ADC Enable */
-    ADCSRA |= (0x01 << ADEN);
-    /* ADIE: Interrupt Enable */
-    ADCSRA |= (0x01 << ADIE);
+        /* ADPS2:0: ADC Prescaler Select Bits 156.25KHz sample rate @ 20MHz */
+        ADCSRA = (0x01 << ADPS2) | (0x01 << ADPS1) | (0x01 << ADPS0);
+        /* ADEN: ADC Enable */
+        ADCSRA |= (0x01 << ADEN);
+        /* ADIE: Interrupt Enable */
+        //ADCSRA |= (0x01 << ADIE);
+}
+uint8_t ADC_value_8bit(uint8_t source)
+{
+    source &= 0b00000111;
+    source |= (ADMUX & 0b11100000);
+    ADMUX = source;
+    uint8_t data = 0;
+
+    //    _delay_ms(2);
+    /* Sleep Enable */
+    SMCR |= (0x01 << SE);
+    __asm__("SLEEP");
+
+    data = ADCL; /* ADCL must be read first, then ADCH ref 23.9.3 */
+    return ADCH;
 }
 uint16_t ADC_value(uint8_t source)
 {
